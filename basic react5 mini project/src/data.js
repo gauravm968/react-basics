75602e10650f185d336c05011c1c49c93aa72dd4
gauravm968 <gauravmahour869@gmail.com>
2023-08-24 19:49:29 +0530

    testimonial mini project in basic react

---
+++ basic react5 mini project/src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Sachin Nigam",
+        job: "Software Developer",
+        image: "https://static.vecteezy.com/system/resources/thumbnails/021/770/044/small/avatar-of-a-myanmar-student-character-free-vector.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project."
+    },
+    {
+        id: 2,
+        name: "Abhishek Uchchriya",
+        job: "Web Developer",
+        image: "https://static.vecteezy.com/system/resources/thumbnails/021/770/044/small/avatar-of-a-myanmar-student-character-free-vector.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project. "
+    },
+    {
+        id: 3,
+        name: "Avadhesh Bhadauriya",
+        job: "Graphic Designer",
+        image: "https://static.vecteezy.com/system/resources/thumbnails/021/770/044/small/avatar-of-a-myanmar-student-character-free-vector.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget."
+    },
+    {
+        id: 4,
+        name: "Monika Sharma",
+        job: "Content Strategist",
+        image: "https://static.vecteezy.com/system/resources/thumbnails/021/770/044/small/avatar-of-a-myanmar-student-character-free-vector.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results."
+    },
+];
+
+export default reviews;
